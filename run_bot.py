85652de5f84af32eef459546b122d4e5ff5331a8
Faxmachinen <github@faxmachinen.com>
2023-03-06 00:52:40 +0100

    Moved feed update code to own file.
    Added authorization.

--- run_bot.py
+++ run_bot.py
@@ -7,13 +7,13 @@ from collections import defaultdict
 
 import whoosh.query
 import whoosh.qparser
-import xml.etree.ElementTree as ET
 from discord.ext import tasks
 import requests
 from bs4 import BeautifulSoup
 
 import steamnews.config
 import steamnews.index
+import steamnews.feeds
 
 class SteamApps:
     @classmethod
@@ -30,7 +30,7 @@ class SteamApps:
             return None
         return results[0]['name']
     def search_names(self, name):
-        parser = whoosh.qparser.QueryParser('name', schema=self.index.schema)
+        parser = whoosh.qparser.QueryParser('name', schema=self.index.schema, group=whoosh.qparser.AndGroup)
         query = parser.parse(name)
         with self.index.searcher() as searcher:
             results = []
@@ -65,11 +65,11 @@ class ProgramState:
             print("Loading previous state...")
             with open(state_file, 'rb') as fh:
                 loaded = pickle.load(fh)
-            print(f"State loaded: {len(loaded.servers)} servers and {len(loaded.last_guids)} feeds")
+            print(f"State loaded: {len(loaded.servers)} servers and {len(loaded.timestamps)} feeds")
             return loaded
     def __init__(self):
         self.servers = {}
-        self.last_guids = {}
+        self.timestamps = {}
     def save(self, config):
         state_file = Path(config['state_file']).absolute()
         with open(state_file, 'wb') as fh:
@@ -82,77 +82,53 @@ class ProgramState:
             self.servers[guild_id] = Server(context)
             print(f"Server #{guild_id} added. Total servers: {len(self.servers)}")
         return self.servers[guild_id]
-    def check_feeds(self, steamapps, config):
-        print("Checking feeds...")
-        feed_ids = defaultdict(list)
-        result = []
+    def get_active_server_feeds(self):
+        feed_servers = defaultdict(list)
         for server in self.servers.values():
             if server.channel is not None:
                 for server_feed in server.subscribed:
-                    feed_ids[server_feed].append(server)
-        print(f"{len(feed_ids)} feeds to check.")
-        for feed_id in feed_ids.keys():
+                    feed_servers[server_feed].append(server)
+        return feed_servers
+    def check_feeds(self, steamapps, config):
+        print("Checking feeds...")
+        feed_servers = self.get_active_server_feeds()
+        print(f"{len(feed_servers)} feeds to check.")
+        result = []
+        for app_id in feed_servers.keys():
             try:
-                new = self.get_new_items(feed_id, steamapps, config)
+                items = steamnews.feeds.load(app_id, config)
+                if app_id not in self.timestamps:
+                    # Feed not seen before, only get latest item.
+                    new = items[-1:]
+                else:
+                    # Feed seen before, get new items.
+                    new = steamnews.feeds.items_after(items, self.timestamps[app_id])
                 if new:
-                    result.append((feed_ids[feed_id], new))
+                    app_name = steamapps.name_from_id(app_id) or '<Unknown>'
+                    result.append((feed_servers[app_id], app_id, app_name, new))
+                    self.timestamps[app_id] = new[-1].timestamp()
             except Exception as ex:
-                print(f"Error getting feed #{feed_id}: {ex}")
+                print(f"Error getting feed #{app_id}: {ex}")
         return result
-    def get_new_items(self, feed_id, steamapps, config):
-        last_guid = self.last_guids.get(feed_id, None)
-        app_name = steamapps.name_from_id(feed_id)
-        url = config['steam_feed_url'].format(id=feed_id)
-        r = requests.get(url)
-        if r.status_code != 200:
-            print(f"Code {r.status_code} when fetching {url}")
-            return None
-        root = ET.fromstring(r.text)
-        link_tag = root.find('channel/link')
-        item_tags = root.findall('channel/item')
-        if len(item_tags) == 0:
-            return []
-        if last_guid is None:
-            item_tags = item_tags[:1]  # Only get the last one
-        items = []
-        for item_tag in item_tags:
-            item = self.parse_item_tag(item_tag, feed_id, app_name, link_tag.text if link_tag else None)
-            if item['guid'] == last_guid:
-                break
-            items.append(item)
-        if items:
-            self.last_guids[feed_id] = items[0]['guid']
-        return items
-    def parse_item_tag(self, item_tag, app_id, app_name, feed_link):
-        return {
-            'guid': item_tag.find('guid').text,
-            'app_id': app_id,
-            'app_name': app_name,
-            'feed_link': feed_link,
-            'item_title': item_tag.find('title').text,
-            'item_link': item_tag.find('link').text,
-            'item_date': item_tag.find('pubDate').text,
-            'item_description': item_tag.find('description').text,
-        }
 
-def blurbify(markup, more_url):
+def blurbify(markup):
     soup = BeautifulSoup(markup, features="html.parser")
     text = soup.get_text(' ')
     if len(text) > 400:
         text = text[:400] + '...'
     return text
 
-def embed_from_feed_item(item, config):
+def embed_from_feed_item(item, app_id, app_name, config):
     embed = discord.Embed(
-        title=escape(item['item_title']),
-        description=escape(f"{item['app_name']} (#{item['app_id']})"),
+        title=escape(item.title),
+        description=escape(f"{app_name} (#{app_id})"),
         color=discord.Colour.fuchsia())
-    embed.set_thumbnail(url=config['steam_app_icon_url'].format(id=item['app_id']))
-    embed.add_field(name=escape(item['item_date']), value=escape(blurbify(item['item_description'], item['item_link'])), inline=False)
-    embed.add_field(name='Read more', value=item['item_link'], inline=False)
+    embed.set_thumbnail(url=config['steam_app_icon_url'].format(id=app_id))
+    embed.add_field(name=item.format_date(), value=escape(blurbify(item.description)), inline=False)
+    embed.add_field(name='Read more', value=item.link, inline=False)
     return embed
 
-async def send_button_message(ctx, message, options, callback):
+async def send_button_message(ctx, message, options, callback, **args):
     class ButtonView(discord.ui.View):
         async def on_timeout(self):
             for child in self.children:
@@ -160,72 +136,95 @@ async def send_button_message(ctx, message, options, callback):
     def make_button(index, option):
         @discord.ui.button(label=option, style=discord.ButtonStyle.primary, row=index//5)
         async def onclick(self, button, interaction):
-            button.disabled = True
             await callback(index, option, interaction)
         setattr(ButtonView, f'button_callback{index}', onclick)
     for index, option in enumerate(options[:25]):
         make_button(index, option)
     class ButtonView2(ButtonView):
         pass
-    await ctx.respond(message, view=ButtonView2(timeout=120))
+    await ctx.respond(message, view=ButtonView2(timeout=120), **args)
 
 def create_bot(config, program_state, steam_app_list):
     steamnewsgroup = discord.SlashCommandGroup(config['bot_name'].lower(), f"Commands for the {config['bot_name']} bot.")
     bot = discord.Bot()
     bot.add_application_command(steamnewsgroup)
 
+    def _authorized(ctx):
+        guild = ctx.guild
+        if not guild:
+            return True
+        return guild.owner.id == ctx.user.id
+
     async def _do_add(ctx, response_func, appid, name):
-        server = program_state.get_server(ctx)
-        channel_was_set = server.add_feed(appid, ctx.channel_id)
-        print(f'[{server.name}#{server.id}] Added "{name}" (#{appid})')
-        msg = f"Ok! I'll give you news about *{escape(name)}* (#{appid})"
-        if channel_was_set:
-            msg += "\nI'll post in this channel."
-        await response_func(msg)
-        if appid not in program_state.last_guids:
-            update_feeds.restart()
-
-    @steamnewsgroup.command(description="Tell the bot to post here.")
-    async def posthere(ctx):
-        server = program_state.get_server(ctx)
-        if not server:
-            await ctx.respond("I can't post here! Ask me in a channel on your server.")
-            return
-        server.channel = int(ctx.channel_id)
-        print(f"[{server.name}#{server.id}] Setting channel to {ctx.channel} ({ctx.channel_id})")
-        await ctx.respond("Ok, I will post here!")
-
-    @steamnewsgroup.command(description="Stop posting to this server.")
-    async def mute(ctx):
         server = program_state.get_server(ctx)
         if not server:
             await ctx.respond("This is not a server!")
             return
+        channel_was_set = server.add_feed(appid, ctx.channel_id)
+        print(f'[{server.name}#{server.id}] Added "{name}" (#{appid})')
+        msg = f"Now posting news about *{escape(name)}* (#{appid})"
+        if channel_was_set:
+            msg += "\nPosting news in this channel."
+        await response_func(msg)
+        if appid not in program_state.timestamps:
+            update_feeds.restart()
+
+    @steamnewsgroup.command(description="Tell the bot to post here.")
+    async def posthere(ctx):
+        if not _authorized(ctx):
+            ctx.respond("Only the server owner can use this command.")
+            return
+        server = program_state.get_server(ctx)
+        if not server:
+            await ctx.respond("This is not a server!")
+            return
+        server.channel = int(ctx.channel_id)
+        print(f"[{server.name}#{server.id}] Setting channel to {ctx.channel}#{ctx.channel_id}")
+        await ctx.respond("Now posting in this channel.")
+
+    @steamnewsgroup.command(description="Stop posting to this server.")
+    async def mute(ctx):
+        if not _authorized(ctx):
+            ctx.respond("Only the server owner can use this command.")
+            return
+        server = program_state.get_server(ctx)
+        if not server:
+            await ctx.respond("This is not a server!")
+            return
+        if server.channel is None:
+            await ctx.respond("I was already not posting anywhere.", ephemeral=True)
+            return
         print(f'[{server.name}#{server.id}] Stopping')
-        await ctx.respond("Ok! I'll stop posting.")
+        await ctx.respond("Ok, I've stopped posting.", ephemeral=True)
+        await bot.get_channel(server.channel).send("No longer posting to this channel.")
         server.channel = None
 
     @steamnewsgroup.command(description="Add a Steam game to the news feed.")
     async def add(ctx, name: str):
+        if not _authorized(ctx):
+            ctx.respond("Only the server owner can use this command.")
+            return
         matches = steam_app_list.search_names(name)
         if len(matches) == 0:
-            await ctx.respond(f"Sorry, I couldn't find *{escape(name)}*!")
+            await ctx.respond(f"Sorry, I couldn't find *{escape(name)}*!", ephemeral=True)
         elif len(matches) > 1:
             labels = [escape(m[1]) for m in matches]
             async def callback(index, label, interaction):
                 appid, name = matches[index]
                 await _do_add(ctx, interaction.response.send_message, appid, name)
-            await send_button_message(ctx, "Is it one of these?", labels, callback)
+            await send_button_message(ctx, "Is it one of these?", labels, callback, ephemeral=True)
         else:
             appid, name = matches[0]
             await _do_add(ctx, ctx.respond, appid, name)
 
     @steamnewsgroup.command(description="Add a Steam game by ID to the news feed.")
     async def addid(ctx, appid: int):
+        if not _authorized(ctx):
+            ctx.respond("Only the server owner can use this command.")
+            return
         name = steam_app_list.name_from_id(appid)
         if not name:
-            print("Not found.")
-            await ctx.respond(f"Sorry! I don't think #{appid} is valid.")
+            await ctx.respond(f"Sorry! I don't think #{appid} is valid.", ephemeral=True)
         else:
             print(f"Found '{name}' for #{appid}")
             await _do_add(ctx, ctx.respond, appid, name)
@@ -233,30 +232,50 @@ def create_bot(config, program_state, steam_app_list):
     @steamnewsgroup.command(description="List all news feeds.")
     async def list(ctx):
         server = program_state.get_server(ctx)
+        if not server:
+            await ctx.respond("This is not a server!")
+            return
         lines = ["These are the feeds you are subscribed to:"]
         for appid in server.subscribed:
             name = steam_app_list.name_from_id(appid) or '<Unnamed>'
             lines.append(f"  • *{escape(name)}* (#{appid})")
         if len(lines) == 1:
-            await ctx.respond("You haven't subscribed to any feeds yet.")
+            await ctx.respond("You haven't subscribed to any feeds yet.", ephemeral=True)
         else:
-            await ctx.respond('\n'.join(lines))
+            await ctx.respond('\n'.join(lines), ephemeral=True)
 
     @steamnewsgroup.command(description="Remove a news feed by #ID.")
     async def removeid(ctx, appid: int):
+        if not _authorized(ctx):
+            ctx.respond("Only the server owner can use this command.")
+            return
+        server = program_state.get_server(ctx)
+        if not server:
+            await ctx.respond("This is not a server!")
+            return
         name = steam_app_list.name_from_id(appid) or '<Unnamed>'
         if appid in server.subscribed:
             server.subscribed.remove(appid)
             print(f"[{server.name}#{server.id}] Removed {name} ({appid})")
-            await ctx.respond(f"Ok! Won't post about *{escape(name)}* (#{appid}) any more.")
+            await ctx.respond(f"Ok! Won't post about *{escape(name)}* (#{appid}) any more.", ephemeral=True)
+            if server.channel is not None:
+                await bot.get_channel(server.channel).send("No longer posting about *{escape(name)}* (#{appid}) in this channel.")
         else:
             await ctx.respond(f"You're not subscribed to *{escape(name)}* (#{appid}).")
 
     @steamnewsgroup.command(description="Remove all news feeds.")
     async def purge(ctx):
+        if not _authorized(ctx):
+            ctx.respond("Only the server owner can use this command.")
+            return
         server = program_state.get_server(ctx)
+        if not server:
+            await ctx.respond("This is not a server!")
+            return
         server.subscribed.clear()
-        await ctx.respond(f"Ok! All subscriptions are gone.")
+        await ctx.respond(f"Ok! All subscriptions are gone.", ephemeral=True)
+        if server.channel is not None:
+            await bot.get_channel(server.channel).send("No longer posting about anything in this channel.")
 
     @bot.event
     async def on_ready():
@@ -265,11 +284,10 @@ def create_bot(config, program_state, steam_app_list):
 
     @tasks.loop(seconds=config['seconds_between_updates'])
     async def update_feeds():
-        servers_new = program_state.check_feeds(steam_app_list, config)
-        new_count = len(servers_new)
-        print(f"{len(servers_new)} updates to post.")
-        for servers, new in servers_new:
-            embeds = [embed_from_feed_item(x, config) for x in new]
+        servers_new_items = program_state.check_feeds(steam_app_list, config)
+        print(f"{len(servers_new_items)} updates to post.")
+        for servers, app_id, app_name, new_items in servers_new_items:
+            embeds = [embed_from_feed_item(x, app_id, app_name, config) for x in new_items]
             for server in servers:
                 channel = bot.get_channel(server.channel)
                 for embed in embeds:
---
+++ steamnews/feeds.py
@@ -0,0 +1,55 @@
+import dateutil.parser
+import requests
+import xml.etree.ElementTree as ET
+
+def getChildTextOrNone(tag, child):
+	if tag is None:
+		return None
+	child_tag = tag.find(child)
+	if child_tag is None:
+		return None
+	return child_tag.text
+
+class NewsItem:
+	@classmethod
+	def from_tag(Cls, item_tag):
+		title = getChildTextOrNone(item_tag, 'title')
+		link = getChildTextOrNone(item_tag, 'link')
+		description = getChildTextOrNone(item_tag, 'description')
+		date_text = getChildTextOrNone(item_tag, 'pubDate')
+		date = dateutil.parser.parse(date_text)
+		return Cls(title, link, description, date)
+	def __init__(self, title, link, description, date):
+		self.title = title
+		self.link = link
+		self.description = description
+		self.date = date
+	def __lt__(self, other):
+		return self.timestamp() < other.timestamp()
+	def timestamp(self):
+		if self.date is None:
+			return 0
+		else:
+			return self.date.timestamp()
+	def format_date(self):
+		if self.date is None:
+			return ""
+		else:
+			return self.date.strftime("%A, %B %d at %H:%M:%S %Z")
+
+def load(app_id, config):
+	url = config['steam_feed_url'].format(id=app_id)
+	r = requests.get(url)
+	if 200 <= r.status_code < 300:
+		return parse(r.text)
+	else:
+		print(f"Code {r.status_code} when fetching {url}")
+		return None
+
+def parse(rss):
+	root = ET.fromstring(rss)
+	item_tags = root.findall('channel/item')
+	return sorted([NewsItem.from_tag(tag) for tag in item_tags])
+
+def items_after(items, timestamp):
+	return [x for x in items if x.timestamp() > timestamp]
